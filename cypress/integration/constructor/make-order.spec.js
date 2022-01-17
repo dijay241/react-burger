b6b802cdad5429832d649d13314af64b316ac3da
Anton Kolchedantsev <admin@findpets.ru>
2022-01-17 13:07:23 +0300

    Tests

--- cypress/integration/constructor/make-order.spec.js
+++ cypress/integration/constructor/make-order.spec.js
@@ -10,6 +10,11 @@ describe('app works correctly with routes', function() {
     it('should open ingredient details after click on it', function() {
         cy.get('[class^=burger-ingredients_item-link__]').first().click();
         cy.contains('Детали ингредиента');
+        cy.get('[class^=ingredient-details_name__]').should('not.to.match', ':empty');
+        cy.get('[class^=ingredient-details_image__]').should('not.to.match', ':empty');
+        cy.get('[class^=ingredient-details_energy-item__]').find('.text_type_digits-default').should('not.to.match', ':empty');
+        cy.get('[class^=modal_close__]').click();
+        cy.contains('Детали ингредиента').should('not.exist');
     });
 
     it('should login user', function() {
@@ -24,10 +29,8 @@ describe('app works correctly with routes', function() {
         cy.get('#bun').find('[class^=burger-ingredients_item__]').first().as('bun');
         cy.get('.bunTarget').first().as('bunTarget');
         cy.get('@bun').drag('@bunTarget');
-
         cy.get('#sauce').find('[class^=burger-ingredients_item__]').first().as('sauce');
         cy.get('@sauce').drag('[class^=burger-constructor_content__]');
-
         cy.get('#main').find('[class^=burger-ingredients_item__]').first().as('main');
         cy.get('@main').drag('[class^=burger-constructor_content__]');
     });
@@ -35,5 +38,8 @@ describe('app works correctly with routes', function() {
     it('should open order details after button click', function() {
         cy.contains('Оформить заказ').click();
         cy.contains('идентификатор заказа');
+        cy.get('[class^=order-details_number__]').should('not.to.match', ':empty');
+        cy.get('[class^=modal_close__]').click();
+        cy.contains('идентификатор заказа').should('not.exist');
     });
 });
